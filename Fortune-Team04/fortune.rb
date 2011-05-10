90d134d796e5003fb0b6a70d49b6956541e2fa84
Brian Collins <collinbc@rose-hulman.edu>
2011-05-10 17:59:53 -0400

    'Task 6 commit'

--- Fortune-Team04/fortune.rb
+++ Fortune-Team04/fortune.rb
@@ -2,4 +2,10 @@ require 'sinatra/base'
 require 'haml'
 require 'thin'
 class Fortune < Sinatra::Base
+get '/fortune/random/?' do
+  @fortune = $fortunes[rand($fortunes.size)]
+  haml :fortune
 end
+end
+
+require 'fortunedata'
