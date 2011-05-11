0b8b8d2e22a2b3653e1b4da81b1c355af44f6719
Brian Collins <collinbc@rose-hulman.edu>
2011-05-10 23:31:47 -0400

    glitchy now

---
+++ Fortune-Team04/features/step_definitions/fortune_steps.rb
@@ -0,0 +1,9 @@
+Then /^I should see a fortune$/ do
+  fortune = page.find(".fortune")
+  fortune.text.should match /^\n.*\n\s*$/
+end
+
+Then /^I should see a fortune with ID$/ do
+  fortune = page.find(".fortune")
+  fortune.text.should match "Your many hidden talents will become obvious to those around you."
+end
